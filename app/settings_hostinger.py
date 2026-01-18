3a595cfca7aff6bc4408efae3b8c75d935258dae
Renato Del Guerso <delguersojr@gmail.com>
2026-01-18 16:07:34 -0300

    Carregar signals de usuarios

--- app/settings_hostinger.py
+++ app/settings_hostinger.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'usuarios',
+    'usuarios.apps.UsuariosConfig',
     'alunos',
     'turmas',
     'ct',
